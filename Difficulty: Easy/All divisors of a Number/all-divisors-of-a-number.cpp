2f2dea128cc414d62a30216e6873dd46cf6dccb0
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2025-10-26 13:53:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void print_divisors(int n) {
+        vector<int> ans;
+        
+        for(int i=1; i<=sqrt(n); i++)
+        {
+            if(n%i==0)
+            {
+                ans.push_back(i);
+                
+                if(n/i!=i)
+                {
+                    ans.push_back(n/i);
+                    
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        for(int &i : ans)
+        {
+            cout<<i<<" ";
+        }
+    }
+};
